2e4c7daafff90b85f420e9eb3af98fecb8ae8f3c
nikiiigoyal <nikiiigoyal@gmail.com>
2024-06-26 09:45:29 +0530

    added js to word freq counter

--- wordFrequencyCounter/script.js
+++ wordFrequencyCounter/script.js
@@ -1,12 +1,24 @@
 
 const textBox = document.querySelector("#textbox")
-const countButton = document.querySelector("#countButton")
+const wordSpan = document.querySelector("#words")
+const characSpan = document.querySelector("#Characters")
+
 
 textBox.addEventListener("input", function () {
     if (textBox.value === "") {
         alert("Please enter text");
     } else {
-        const inputTextValue = textBox.value;
-        console.log("Input Text:", inputTextValue);
+        const inputTextValue = textBox.value.trim(); //to not count extra spaces in starting and ending
+        const char = inputTextValue.length  //to get length of text
+        characSpan.innerHTML = char;
+        
+        
+        let words = inputTextValue.split(" ")
+        let cleanSpaces = words.filter(word => word !== "") // to remove extra spaces from between
+        wordSpan.innerHTML = cleanSpaces.length;
+        
     }
+
 });
+
+
