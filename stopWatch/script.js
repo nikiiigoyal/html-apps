753ea59873c8333ca0add789bbf790eb3469135a
nikiiigoyal <nikiiigoyal@gmail.com>
2024-06-27 13:53:42 +0530

    added javascript project to stopwatch

---
+++ stopWatch/script.js
@@ -0,0 +1,44 @@
+
+let displayTime = document.querySelector("#time")
+const timer = null;
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
+const resetBtn = document.querySelector("#reset")
+
+let [hours ,minutes, seconds] = [0, 0, 0];
+
+function stopWatchTime () {
+    seconds++ 
+    if (seconds === 60) {
+        seconds= 0;
+        minutes++
+        if(minutes === 60) {
+            minutes = 0
+            hours++
+        }
+        console.log("hours")
+    }
+    let h = hours < 10 ? "0" + hours: hours;
+    let m = minutes < 10 ? "0" + minutes: minutes;
+    let s = seconds < 10 ? "0" + seconds: seconds;
+    displayTime.innerHTML = h + ":"+ m +":"+ s
+}
+function watchStart() {
+ if(timer!= null) {
+    clearInterval(timer)
+ }
+ timer = setInterval(stopWatchTime, 1000)
+}
+function watchStop() {
+    clearInterval(timer)
+}
+function watchReset() {
+    clearInterval(timer)
+    [hours ,minutes, seconds] =[0, 0, 0];
+    displayTime.innerHTML = "00:00:00"
+
+}
+startBtn.addEventListener("click", watchStart)
+stopBtn.addEventListener("click", watchStop)
+resetBtn.addEventListener("click", watchReset)
+
