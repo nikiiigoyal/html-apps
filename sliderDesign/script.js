b52d422016b24f3ce0e1121761b1278cfaba1b0f
nikiiigoyal <nikiiigoyal@gmail.com>
2024-06-30 12:41:27 +0530

    added js to new project

--- sliderDesign/script.js
+++ sliderDesign/script.js
@@ -1,23 +1,34 @@
-const previousBtn = document.querySelector("#previous")
-const nextBtn = document.querySelector("#next")
-let imageNumber =  1
+const slides = document.querySelectorAll('.slide');
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+
+let currentSlide = 0;
+
+//displaying only one slide at a time
 function slideshow() {
-    const slides = document.querySelectorAll(".slide") //getting all ths slides by common class name
-    slides[imageNumber].style.display = "block"
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active'); // Removing "active" class from all slides:
+        console.log(slides)
+      }
+      slides[currentSlide].classList.add('active'); //adding active class
+      console.log(slides)
+    }
+      // Show the first slide initially
+         slideshow(currentSlide);
 
 
-console.log(slides)
-}
-slideshow()
-function controller(x) {
- imageNumber = imageNumber + x;
-}
 
-previousBtn.addEventListener("click",function() {
-    controller(-1)
-    
+previousButton.addEventListener("click",function() {
+
+    if (currentSlide === slides.length - 1) {
+        currentSlide = 0; // Set to first slide if on last slide
+      } else {
+        currentSlide++;
+    }
 })
- nextBtn.addEventListener("click",function() {
-    controller(1)
+ nextButton.addEventListener("click",function() {
+    currentSlide--
+    currentSlide = currentSlide < 0 ? slides.length - 1 : currentSlide;
+  slideshow(currentSlide);
 })
 
