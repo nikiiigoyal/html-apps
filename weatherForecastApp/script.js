4e269f62082b8bd5b4350f3412b0f02391c289fb
nikiiigoyal <nikiiigoyal@gmail.com>
2024-08-03 18:38:16 +0530

    new project

--- weatherForecastApp/script.js
+++ weatherForecastApp/script.js
@@ -3,13 +3,15 @@ const input = document.querySelector(".top-banner input");
 const list = document.querySelector("#cities");
 const button = document.getElementById("submit-btn");
 const container = document.querySelector("#ajax-container");
+const sideBar = document.querySelector("#sidebar");
 const weatherApi = "https://goweather.herokuapp.com/weather/Curitiba";
 
+// add event listener
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   getWeatherData();
 });
-
+// function to get data from Api
 async function getWeatherData() {
   const inputValue = input.value;
   console.log(inputValue);
@@ -34,13 +36,15 @@ async function getWeatherData() {
       console.log(wind);
 
       displayWeather(inputValue, description, temperature, wind);
+      addRecentSearch(inputValue);
+      displayRecentSearches();
     } catch (error) {
       console.error("Error fetching weather data:", error);
       // Here you would typically show an error message to the user
     }
   }
 }
-
+// function to display the data we got
 function displayWeather(city, description, temperature, wind) {
   const iconClass = getWeatherIcon(description);
   const li = document.createElement("li");
@@ -58,6 +62,7 @@ function displayWeather(city, description, temperature, wind) {
     container.appendChild(list);
   }
 }
+// function for the icons
 function getWeatherIcon(description) {
   const lowercaseDesc = description.toLowerCase();
   if (lowercaseDesc.includes("sun") || lowercaseDesc.includes("clear")) {
@@ -78,4 +83,33 @@ function getWeatherIcon(description) {
   } else {
     return "fas fa-cloud-sun"; // default icon
   }
+}
+//function to get recent searches
+function getRecentSearches() {
+  return JSON.parse(localStorage.getItem("recentSearches")) || [];
+}
+
+//function to store data in a local storage
+
+function addRecentSearch(city) {
+  let searches = JSON.parse(localStorage.getItem("recentSearches")) || [];
+  searches = searches.filter((item) => item !== city);
+  searches.unshift(city);
+  searches = searches.slice(0, 4);
+  localStorage.setItem("recentSearches", JSON.stringify(searches));
+}
+//function to display recent searches
+function displayRecentSearches() {
+  const searches = getRecentSearches();
+  const sideBarList = document.createElement("ul");
+  sideBarList.classList.add("recent-searches");
+
+  searches.forEach((city) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span class = "city-name">${city}</span>`;
+    sideBarList.appendChild(li);
+  });
+  sideBar.appendChild(sideBarList);
+  console.log(sideBar);
 }
