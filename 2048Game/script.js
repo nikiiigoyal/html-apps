5bbb6ba84976f4f1e335545206ce924a5b695ba3
nikiiigoyal <nikiiigoyal@gmail.com>
2024-09-06 22:08:54 +0530

    new project

---
+++ 2048Game/script.js
@@ -0,0 +1,4 @@
+let scoreContainer = document.querySelector{".score-container"}
+let bestContainer = document.querySelector{".best-container"}
+let newGame = document.querySelector{"#restartBtn"}
+let gridContainer = document.querySelector{".grid-container"}
